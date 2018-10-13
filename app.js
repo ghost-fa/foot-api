2bf2927be699de276829e036203b005e21b45fc1
ghost-fk <fayyad_r22@yahoo.com>
2018-10-13 10:38:39 +0200

    generic error handler  and catch the undefined routes

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const Boom = require('boom')
 
 
-var usersRouter = require('./routes/users');
+var teamsRouter = require('./routes/teams');
 
 var app = express();
 
@@ -15,6 +16,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/users', usersRouter);
+app.use('/api/v1/teams', teamsRouter);
+
+// Catch undefined routes
+app.use((req, res, next) =>{
+  const err = Boom.notFound(`sorry cant found the path ${req.path}`)
+
+  next(err)
+});
+
+// Generic error handler
+app.use((req, res, next) =>{
+  let status, message;
+  if(err.output){
+    status = err.output.statusCode;
+    message = err.output.payload;
+  }else {
+    status =err.status || 500;
+    message = {
+      message: err.message || 'Oops, something bad happened'
+    }
+  }
+    res.status(status)
+      .join(message)
+
+})
 
 module.exports = app;
